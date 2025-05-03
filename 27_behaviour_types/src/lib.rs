e05db06753e83aa0828d4d834b3edb3401860ddc
ta4h1r <taahirbhaiyat01@gmail.com>
2025-05-03 23:00:03 +0200

    Add alternative blog post structure

---
+++ 27_behaviour_types/src/lib.rs
@@ -0,0 +1,47 @@
+// Alternative to using 26_state_pattern for blog a post structure. 
+// The only way to get a published Post instance that does have a content method defined is
+// to call the approve method on a PendingReviewPost
+
+pub struct Post {
+    content: String,
+}
+
+pub struct DraftPost {
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> DraftPost {
+        DraftPost {
+            content: String::new(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
+        }
+    }
+}
+
+pub struct PendingReviewPost {
+    content: String,
+}
+
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
+    }
+}
---
+++ book.sh
@@ -0,0 +1,1 @@
+rustup docs --book
